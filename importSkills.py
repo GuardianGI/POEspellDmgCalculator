3dd4ff1563090537715d9e3f6a7fe3b70f24019e
Martin Kaas <ggimkaas@gmail.com>
2014-07-22 17:38:24 +0200

    removed old log line, modified scrapping script to get all support gems (going do add attack support gems in the following commits)

--- importSkills.py
+++ importSkills.py
@@ -1,14 +1,16 @@
 from urllib.request import Request, urlopen
 from json import load
 
+dir = "E:\\programming\\Node\\POESkillTree\\"
+
 def getPage(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     return urlopen(req)
 
-f_spells = open("spellSupports.json")
+f_spells = open(dir + "spellSupports.json")
 for spell in load(f_spells)["supports"] :
 	HTML = getPage("http://pathofexile.gamepedia.com{}".format(spell["href"]))
-	f_spell = open("spellSupportsHTML/{}.html".format(spell["name"].replace("/", "")), "wb")
+	f_spell = open(dir + "spellSupportsHTML/{}.html".format(spell["name"].replace("/", "")), "wb")
 	f_spell.write(HTML.read())
 	f_spell.close()
 f_spells.close()
